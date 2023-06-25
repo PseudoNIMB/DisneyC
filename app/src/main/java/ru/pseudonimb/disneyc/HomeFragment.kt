b8ca0c776b9b8579c10462b3761b72c3f3ee8511
PseudoNIMB <ienndevld@yandex.ru>
2023-06-25 15:04:01 +0300

    Навигация и фрагменты

--- app/src/main/java/ru/pseudonimb/disneyc/HomeFragment.kt
+++ app/src/main/java/ru/pseudonimb/disneyc/HomeFragment.kt
@@ -1,17 +1,7 @@
 package ru.pseudonimb.disneyc
 
 import android.os.Bundle
-import android.view.View
+
 
 class HomeFragment {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        button_next.setOnClickListener {
-            (activity as MainActivity).navController.navigate(R.id.home_fragment_root)
-        }
-
-        button_prev.setOnClickListener {
-            (activity as MainActivity).navController.popBackStack()
-        }
-    }
 }
--- app/src/main/java/ru/pseudonimb/disneyc/SettingsFragment.kt
+++ app/src/main/java/ru/pseudonimb/disneyc/SettingsFragment.kt
@@ -1,17 +1,4 @@
 package ru.pseudonimb.disneyc
 
-import android.os.Bundle
-import android.view.View
-
 class SettingsFragment {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        button_next.setOnClickListener {
-            (activity as MainActivity).navController.navigate(R.id.settings_fragment_root)
-        }
-
-        button_prev.setOnClickListener {
-            (activity as MainActivity).navController.popBackStack()
-        }
-    }
 }
