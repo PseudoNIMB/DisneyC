7f05811118006760758e94bd00deeb69ad031986
PseudoNIMB <ienndevld@yandex.ru>
2023-06-25 15:45:03 +0300

    Конечная станция перед третьим спринтом

--- app/src/main/java/ru/pseudonimb/disneyc/FavorFragment.kt
+++ app/src/main/java/ru/pseudonimb/disneyc/FavorFragment.kt
@@ -1,7 +1,23 @@
 package ru.pseudonimb.disneyc
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.pseudonimb.disneyc.databinding.FragmentFavorBinding
+
 
 class FavorFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentFavorBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
 
 }
--- app/src/main/java/ru/pseudonimb/disneyc/SettingsFragment.kt
+++ app/src/main/java/ru/pseudonimb/disneyc/SettingsFragment.kt
@@ -1,7 +1,22 @@
 package ru.pseudonimb.disneyc
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.pseudonimb.disneyc.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentSettingsBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
 
 }
