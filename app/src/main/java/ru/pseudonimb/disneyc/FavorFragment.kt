ca94443d5751f4c1bc725fafe8c74eee942d8074
PseudoNIMB <ienndevld@yandex.ru>
2023-06-25 15:46:37 +0300

    Чистка

--- app/src/main/java/ru/pseudonimb/disneyc/FavorFragment.kt
+++ app/src/main/java/ru/pseudonimb/disneyc/FavorFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.pseudonimb.disneyc.databinding.FragmentFavorBinding
 
-
 class FavorFragment : Fragment() {
 
     override fun onCreateView(
--- app/src/main/java/ru/pseudonimb/disneyc/HomeFragment.kt
+++ app/src/main/java/ru/pseudonimb/disneyc/HomeFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.pseudonimb.disneyc.databinding.FragmentHomeBinding
-import ru.pseudonimb.disneyc.databinding.FragmentSettingsBinding
-
 
 class HomeFragment : Fragment() {
 
