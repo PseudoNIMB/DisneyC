6f37ca6a4927cdc3401b1ed6e30c6af2edc5ffba
PseudoNIMB <ienndevld@yandex.ru>
2023-06-26 18:36:06 +0300

    Создание класса RV адаптера

---
+++ app/src/main/java/ru/pseudonimb/disneyc/CharListRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package ru.pseudonimb.disneyc
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CharListRecyclerAdapter (private val clickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>()  {
+    //Здесь у нас хранится список элементов для RV
+    private val items = mutableListOf<Char>()
+
+    //Этот метод нужно переопределить на возврат количества элементов в списке RV
+    override fun getItemCount() = items.size
+
+    //В этом методе мы привязываем наш ViewHolder и передаем туда "надутую" верстку нашего фильма
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return CharViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.char_item, parent, false))
+    }
+
+    //В этом методе будет привязка полей из объекта Film к View из film_item.xml
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is CharViewHolder -> {
+
+            }
+        }
+    }
+
+    //Метод для добавления объектов в наш список
+    fun addItems(list: List<Char>) {
+        //Сначала очищаем(если не реализовать DiffUtils)
+        items.clear()
+        //Добавляем
+        items.addAll(list)
+        //Уведомляем RV, что пришел новый список, и ему нужно заново все "привязывать"
+        notifyDataSetChanged()
+    }
+
+
+    //Интерфейс для обработки кликов
+    interface OnItemClickListener {
+        fun click(film: Char)
+    }
+
+}
---
+++ app/src/main/java/ru/pseudonimb/disneyc/CharViewHolder.kt
@@ -0,0 +1,9 @@
+package ru.pseudonimb.disneyc
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CharViewHolder (private val
+                      itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
