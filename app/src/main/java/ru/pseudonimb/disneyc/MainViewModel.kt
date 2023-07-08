29af62a0a7df05eb1027e8af7fc004be457f736c
PseudoNIMB <ienndevld@yandex.ru>
2023-07-08 16:52:23 +0300

    Создание ViewModel

---
+++ app/src/main/java/ru/pseudonimb/disneyc/MainViewModel.kt
@@ -0,0 +1,9 @@
+package ru.pseudonimb.disneyc
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+}
